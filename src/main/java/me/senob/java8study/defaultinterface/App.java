04d6fb1b37a145a37dc1a44dd7733f5b86f2bd6b
seno-b <bangshinho@gmail.com>
2021-11-23 22:07:48 +0900

    feat: 2. 인터페이스의 변화 - 자바 8 API의 기본 메소드와 스태틱 메소드

--- src/main/java/me/senob/java8study/defaultinterface/App.java
+++ src/main/java/me/senob/java8study/defaultinterface/App.java
@@ -1,5 +1,8 @@
 package me.senob.java8study.defaultinterface;
 
+import java.util.*;
+import java.util.function.Consumer;
+
 public class App {
 
     public static void main(String[] args) {
@@ -8,5 +11,32 @@ public class App {
         defaultFoo.printName();
         defaultFoo.printNameUppercase();
         Foo.printAnythings();
+
+        List<String> names = new ArrayList<>(Arrays.asList("seno", "taeho", "donghyun", "youngmin"));
+
+        Consumer<String> s = s1 -> {
+            System.out.println("s1 = " + s1);
+        };
+
+        names.forEach(s);
+        names.forEach(System.out::println);
+
+        System.out.println("-----------------------------");
+        Spliterator<String> spliterator = names.spliterator();
+        Spliterator<String> spliterator1 = spliterator.trySplit();
+        while(spliterator.tryAdvance(System.out::println));
+        System.out.println("-----------------------------");
+        while(spliterator1.tryAdvance(System.out::println));
+
+        names.removeIf(n -> n.startsWith("s"));
+        names.forEach(System.out::println);
+
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        names.sort(compareToIgnoreCase);
+        names.forEach(System.out::println);
+
+        System.out.println("--------------------------------");
+        names.sort(compareToIgnoreCase.reversed());
+        names.forEach(System.out::println);
     }
 }
