1560ad5c5497773754571914c2816a1f782b6005
senob <bangshinho@gmail.com>
2021-12-15 23:10:14 +0900

    feat: CompletableFuture - CompletableFuture 1

---
+++ src/main/java/me/senob/java8study/completable/App.java
@@ -0,0 +1,24 @@
+package me.senob.java8study.completable;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class App {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        CompletableFuture<Void> stringCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "string";
+        }, executorService
+        ).thenRunAsync(() -> System.out.println("그냥 뭔가 실행 ... " + Thread.currentThread().getName()), executorService);
+
+        stringCompletableFuture.get();
+    }
+}
