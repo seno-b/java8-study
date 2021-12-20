d278a87475379ec95aa787ff80686e4317209103
senob <bangshinho@gmail.com>
2021-12-20 23:40:48 +0900

    test: thread single test

--- src/main/java/me/senob/java8study/completable/App.java
+++ src/main/java/me/senob/java8study/completable/App.java
@@ -7,17 +7,14 @@ import java.util.concurrent.Executors;
 
 public class App {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
-        CompletableFuture<Void> stringCompletableFuture = CompletableFuture.supplyAsync(() -> {
-            System.out.println(Thread.currentThread().getName());
-            try {
-                Thread.sleep(3000L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return "string";
-        }, executorService
-        ).thenRunAsync(() -> System.out.println("그냥 뭔가 실행 ... " + Thread.currentThread().getName()), executorService);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        executorService.submit(
+                () -> {
+                    return "test";
+                }
+        );
+
 
     }
 }
