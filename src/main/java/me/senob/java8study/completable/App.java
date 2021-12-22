80278c23e38d424a0c86423c9f6d46121e6e9418
senob <bangshinho@gmail.com>
2021-12-22 23:00:28 +0900

    feat: CompletableFuture - CompletableFuture

--- src/main/java/me/senob/java8study/completable/App.java
+++ src/main/java/me/senob/java8study/completable/App.java
@@ -31,15 +31,52 @@ public class App {
 
 //        System.out.println("voidCompletableFuture.get() = " + voidCompletableFuture.get());
 
-        List<CompletableFuture<String>> completableFutures = Arrays.asList(hello, world);
+//        List<CompletableFuture<String>> completableFutures = Arrays.asList(hello, world);
+//
+//        CompletableFuture<List<String>> results = CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[completableFutures.size()]))
+//                .thenApply(v -> completableFutures.stream()
+//                        .map(CompletableFuture::join)
+//                        .collect(Collectors.toList()));
+//
+//        List<String> futures = results.get();
+//        futures.forEach(System.out::println);
 
-        CompletableFuture<List<String>> results = CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[completableFutures.size()]))
-                .thenApply(v -> completableFutures.stream()
-                        .map(CompletableFuture::join)
-                        .collect(Collectors.toList()));
 
-        List<String> futures = results.get();
-        futures.forEach(System.out::println);
+//        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.anyOf(hello, world).thenAccept(System.out::println);
+//        System.out.println("voidCompletableFuture.get() = " + voidCompletableFuture.get());
+
+        extractCompletableFutureExceptionally();
+    }
+
+    private static void extractCompletableFutureExceptionally() throws ExecutionException, InterruptedException {
+        boolean throwError = true;
+
+        /*CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            if (throwError) {
+                throw new IllegalArgumentException();
+            }
+
+            System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+            return "Hello";
+        }).exceptionally((ex) -> {
+            System.out.println("ex = " + ex);
+            return "Error!!";
+        });*/
+
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            if (throwError) {
+                throw new IllegalArgumentException();
+            }
+
+            System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+            return "Hello";
+        }).handle((result, ex) -> {
+            if (ex != null) {
+                System.out.println("ex = " + ex);
+            }
+            return result;
+        });
+        System.out.println("future = " + future.get());
     }
 
     private static CompletableFuture<String> getWorld(String message) {
